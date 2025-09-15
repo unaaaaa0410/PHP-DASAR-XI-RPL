afd3d141553d39a35f5b86a7cb11867fac80195b
unaaaaa0410 <ludznaluna@gmail.com>
2025-09-15 13:32:49 +0700

    variabel
    sintax variabel

---
+++ variabel/latihan1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>variabel</title>
+</head>
+<body>
+      <?php 
+         $nama="unaaa";
+        
+         echo $nama="unaaa";
+        
+       ?>
+</body>
+</html>
---
+++ variabel/latihan2.php
@@ -0,0 +1,11 @@
+<?php 
+   $nama="unaaa";
+   $kelas="11 rpl";
+   $alamat="tokyo";
+
+   echo "nama : $nama";
+   echo "<br>";
+   echo "kelas : $kelas";
+   echo "<br>";
+   echo "alamat : $alamat";
+ ?>
